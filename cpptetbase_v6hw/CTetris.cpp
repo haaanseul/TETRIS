8207052eca9f231f8501581d67c2e11851ac3a68
haaanseul <9gkstmf@naver.com>
2023-05-09 01:09:29 +0900

    일단 init 보류 어느정도 된 듯?

--- cpptetbase_v6hw/CTetris.cpp
+++ cpptetbase_v6hw/CTetris.cpp
@@ -14,32 +14,32 @@ void CTetris::init(int **setOfColorBlockArrays, int nTypes, int nDegrees) {
 
   Tetris::init(setOfColorBlockArrays, nTypes, nDegrees); // call superclass' function
 
-  numTypes = nTypes;
-  numDegrees = nDegrees;
+  // write the rest of this function!!
 
-  setOfBlockObjects = new Matrix**[numTypes]; // allocate 1d array of pointers to Matrix pointer arrays
+  // setOfColorBlockObjects = setOfBlockObjects; // allocate 1d array of pointers to Matrix pointer arrays
+  setOfColorBlockObjects = new Matrix**[numTypes]; // allocate 1d array of pointers to Matrix pointer arrays
 
   for (int t = 0; t < numTypes; t++)
-    setOfBlockObjects[t] = new Matrix*[numDegrees]; // allocate 1d array of Matrix pointers
-
-  for (int t = 0; t < numTypes; t++) { // 타입 기준 for 문
-    int *array = setOfColorBlockArrays[numDegrees * t]; // 어레이에서 타입 뽑기
-    int idx, size;
-    for (idx = 0; array[idx] != -1 ; idx++); // find the element of -1 in array[] // idx 크기 조절
-    for (size = 0; size*size < idx; size++); // comupte the square root of idx // size 사이즈 측정
-    wallDepth = (size > wallDepth ? size : wallDepth); // ?? 모르겠다
-    for (int d = 0; d < numDegrees; d++) { // allocate matrix objects // 회전 블록 기준
-      int *array2 = new int[size*size+1]; // 블록 메모리 생성
-      int k; 
-      for (k = 0; k < size*size; k++) // 블록 생성, 0이 아닌 다른 수 1로 통일
+    setOfColorBlockObjects[t] = new Matrix*[numDegrees]; // allocate 1d array of Matrix pointers
+  for (int t = 0; t < numTypes; t++) {
+    int *array = setOfColorBlockArrays[numDegrees * t];
+    
+    // int idx, size;
+    // for (idx = 0; array[idx] != -1 ; idx++); // find the element of -1 in array[]
+    // for (size = 0; size*size < idx; size++); // comupte the square root of idx
+    for (int d = 0; d < numDegrees; d++) {
+      Matrix *size_Object = setOfBlockObjects[t][d]; 
+      int size = size_Object->get_dx();
+      int *array2 = new int[size*size+1];
+      int k;
+      for (k = 0; k < size*size; k++){
         array2[k] = setOfColorBlockArrays[numDegrees * t + d][k];
+      }
       array2[k] = -1;
-      setOfBlockObjects[t][d] = new Matrix(array2, size, size);
+      setOfColorBlockObjects[t][d] = new Matrix(array2, size, size);
       delete[] array2;
-      // cout << *setOfBlockObjects[t][d] << endl;
     }
   }
-  // write the rest of this function!!
 }
 
 void CTetris::deinit(void) {
@@ -82,9 +82,6 @@ TetrisState CTetris::accept(char key) {
   TetrisState _state = Tetris::accept(key); // call superclass' function
 
   // write the rest of this function!!
-
-  // you can use the following code if you want to
-  // oCScreen = deleteFullLines(oCScreen, currCBlk, top, wallDepth);
 
   return _state;
 }
